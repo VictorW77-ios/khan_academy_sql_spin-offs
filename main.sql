369fd0d588aadbdd1b619fd1df281d4589bfa02e
Victor Williams <victorwilliams1030@gmail.com>
2020-07-14 13:10:14 +0000

    Updated file names

--- main.sql
+++ main.sql
@@ -12,3 +12,4 @@ CREATE TABLE groceries (id INTEGER PRIMARY KEY, name TEXT, quantity INTEGER );
 INSERT INTO groceries VALUES (1, "Bananas", 4);
 INSERT INTO groceries VALUES (2, "Peanut Butter", 1);
 INSERT INTO groceries VALUES (3, "Dark Chocolate Bars", 2);
+
